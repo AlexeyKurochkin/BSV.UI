56481ac33442bf5ccc4a6a71ce2db3bea9e51bc1
Alexey Kurochkin <Alexey_Kurochkin1@epam.com>
2024-06-01 20:40:12 +0300

    Refactored footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -66,38 +66,43 @@ export default function Footer() {
 						display: "flex",
 						flexDirection: "column",
 						gap: 4,
-						minWidth: { xs: "100%", sm: "60%" },
+						minWidth: { xs: "100%", sm: "25%", md: "20%" },
 					}}
 				>
-					<Box sx={{ width: { xs: "100%", sm: "60%" } }}>
-						<Box sx={{ ml: "-15px" }}>
+					<Box sx={{ width: { xs: "100%", sm: "100%" } }}>
+						<Box>
 							<img src={"Logo.png"} style={logoStyle} alt="logo of sitemark" />
 						</Box>
 						<Typography variant="body2" fontWeight={600}>
-							Newsletter
+							Anastasia Besson
 						</Typography>
 						<Typography variant="body2" color="text.secondary" mb={2}>
-							Subscribe to our newsletter for weekly updates and promotions.
+							<Link target="_blank" href="https://www.instagram.com/nastia.besson/">
+								Contact me
+							</Link>{" "}
+							for any questions.
 						</Typography>
-						<Stack direction="row" spacing={1} useFlexGap>
-							<TextField
-								id="outlined-basic"
-								hiddenLabel
-								size="small"
-								variant="outlined"
-								fullWidth
-								aria-label="Enter your email address"
-								placeholder="Your email address"
-								inputProps={{
-									autoComplete: "off",
-									"aria-label": "Enter your email address",
-								}}
-							/>
-							<Button variant="contained" color="primary" sx={{ flexShrink: 0 }}>
-								Subscribe
-							</Button>
-						</Stack>
 					</Box>
+				</Box>
+				<Box
+					sx={{
+						display: {
+							sm: "flex",
+						},
+						minWidth: 300,
+						width: "100%",
+						minHeight: 300,
+						mx: { xs: 0, sm: 2 },
+					}}
+				>
+					<iframe
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1531.6531103499156!2d25.26578038184219!3d54.67497372168125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd946df8b113b1%3A0xbd4648114fb3a87d!2sVytenio%20g.%2C%20Vilnius%2C%2003229%20Vilniaus%20m.%20sav.!5e0!3m2!1sru!2slt!4v1717261186270!5m2!1sru!2slt"
+						height="300"
+						style={{ border: 0, width: "100%" }}
+						allowFullScreen={false}
+						loading="lazy"
+						referrerPolicy="no-referrer-when-downgrade"
+					></iframe>
 				</Box>
 				<Box
 					sx={{
@@ -145,46 +150,6 @@ export default function Footer() {
 						FAQs
 					</Link>
 				</Box>
-				<Box
-					sx={{
-						display: { xs: "none", sm: "flex" },
-						flexDirection: "column",
-						gap: 1,
-					}}
-				>
-					<Typography variant="body2" fontWeight={600}>
-						Company
-					</Typography>
-					<Link color="text.secondary" href="#">
-						About us
-					</Link>
-					<Link color="text.secondary" href="#">
-						Careers
-					</Link>
-					<Link color="text.secondary" href="#">
-						Press
-					</Link>
-				</Box>
-				<Box
-					sx={{
-						display: { xs: "none", sm: "flex" },
-						flexDirection: "column",
-						gap: 1,
-					}}
-				>
-					<Typography variant="body2" fontWeight={600}>
-						Legal
-					</Typography>
-					<Link color="text.secondary" href="#">
-						Terms
-					</Link>
-					<Link color="text.secondary" href="#">
-						Privacy
-					</Link>
-					<Link color="text.secondary" href="#">
-						Contact
-					</Link>
-				</Box>
 			</Box>
 			<Box
 				sx={{
@@ -197,15 +162,6 @@ export default function Footer() {
 				}}
 			>
 				<div>
-					<Link color="text.secondary" href="#">
-						Privacy Policy
-					</Link>
-					<Typography display="inline" sx={{ mx: 0.5, opacity: 0.5 }}>
-						&nbsp;•&nbsp;
-					</Typography>
-					<Link color="text.secondary" href="#">
-						Terms of Service
-					</Link>
 					<Copyright />
 				</div>
 				<Stack
@@ -218,24 +174,18 @@ export default function Footer() {
 					}}
 				>
 					<IconButton
+						target="_blank"
 						color="inherit"
-						href="https://github.com/mui"
+						href="https://github.com/AlexeyKurochkin"
 						aria-label="GitHub"
 						sx={{ alignSelf: "center" }}
 					>
 						<FacebookIcon />
 					</IconButton>
 					<IconButton
+						target="_blank"
 						color="inherit"
-						href="https://twitter.com/MaterialUI"
-						aria-label="X"
-						sx={{ alignSelf: "center" }}
-					>
-						<TwitterIcon />
-					</IconButton>
-					<IconButton
-						color="inherit"
-						href="https://www.linkedin.com/company/mui/"
+						href="https://www.linkedin.com/in/alexey-kurochkin-dev/"
 						aria-label="LinkedIn"
 						sx={{ alignSelf: "center" }}
 					>
